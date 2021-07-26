cd6653cacda07ccac9a4d04526b10bf606f21275
georgech3 <zx1256910@gmail.com>
2021-07-26 12:29:40 +0800

    Add Header and Footer component to default layout

---
+++ frontend/components/Header.vue
@@ -0,0 +1,11 @@
+<template>
+  <b-navbar type="white" variant="light">
+    <b-container fluid="md">
+      <b-navbar-brand href="#">AglaeCal</b-navbar-brand>
+      <b-navbar-nav class="ml-auto" right>
+        <b-nav-item href="/videos">Videos</b-nav-item>
+        <b-nav-item href="/login">Logout</b-nav-item>
+      </b-navbar-nav>
+    </b-container>
+  </b-navbar>
+</template>
