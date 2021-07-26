1df2ad106739cedb941786eae1c6580097228b4c
georgech3 <zx1256910@gmail.com>
2021-07-26 17:11:17 +0800

    Revise Logo.vue in svg with accessibility

--- frontend/components/Header.vue
+++ frontend/components/Header.vue
@@ -1,9 +1,10 @@
 <template>
   <b-navbar type="white" variant="white">
     <b-container fluid="md">
-      <b-navbar-brand href="/"
-        ><img src="@/assets/img/logo.jpeg" alt="logo of the AlgaeCal"
-      /></b-navbar-brand>
+      <b-navbar-brand href="/">
+        <Logo />
+        <!-- <img src="@/assets/img/logo.jpeg" alt="logo of the AlgaeCal"/> -->
+      </b-navbar-brand>
       <b-navbar-nav class="ml-auto" right>
         <b-nav-item href="/videos">Videos</b-nav-item>
         <b-nav-item href="/login">Logout</b-nav-item>
@@ -12,10 +13,20 @@
   </b-navbar>
 </template>
 
+<script>
+import Logo from "./Logo.vue";
+
+export default {
+  components: {
+    Logo,
+  },
+};
+</script>
+
 <style scoped>
-.navbar-brand > img {
+.navbar-brand > svg {
   height: auto;
   padding: 15px;
-  width: 80px;
+  width: 100px;
 }
 </style>
